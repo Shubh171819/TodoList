944dfc2cd040ef7b7740eeb9dc6bbd9999608532
Shubham Awachar <shubhamawachar17@gmail.com>
2024-12-20 00:21:48 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react'
+
+const App = () => {
+
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const addTasks = () => {
+    if (task !== "") {
+      setTasks([...tasks, task]);
+      setTask("");
+    }
+  }
+
+  const deleteTasks = (index) => {
+    const updatedList = [...tasks];
+    updatedList.splice(index, 1);
+    setTasks(updatedList);
+  }
+
+  return (
+    <div className='flex flex-col items-center'>
+      <h1 className='text-4xl m-16 font-bold'>Simple Todo App</h1>
+
+      <div className='p-6'>
+        <input className='bg-slate-100 rounded-md p-4 m-4' type='text' value={task}
+          onChange={(e) => { setTask(e.target.value) }} placeholder='Create a new todo' />
+
+        <button onClick={addTasks} className='bg-green-500 text-white p-3 m-3 rounded-md 
+         font-bold hover:bg-green-600'>Add Task</button>
+      </div>
+
+      <div>
+        {
+          tasks.length > 0 ?
+            (
+              <ul>
+                {
+                  tasks.map((task, index) => (
+                    <div className='flex bg-slat-100 m-4 py-4 pl-12 pr-4 rounded-md' key={index}>
+                      <li className='self-center font-semibold pr-10 mr-6 grow'>{task}</li>
+                      <button onClick={() => deleteTasks(index)} className='bg-red-500 text-white p-2 mx-1 
+                  rounded-md font-bold hover:bg-red-600'>Delete</button>
+                    </div>
+                  ))
+                }
+              </ul>
+            ) :
+
+            (
+              <div>
+                <p>No Task Found</p>
+              </div>
+            )
+
+        }
+      </div>
+    </div>
+  )
+}
+
+export default App
